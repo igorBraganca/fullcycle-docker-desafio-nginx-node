1434ef27db3d6f687a392eaba125b327ad41db02
Igor Bragança <38175+igorBraganca@users.noreply.github.com>
2023-09-09 19:19:41 -0300

    adding configuration for dev environment

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.get('/', async (req, res) => {
     const connection = await getConnection()
     const [rows, fields] = await connection.query('SELECT name FROM people')
 
-    const response = ['<h1>Full Cycle Rocks!</h1>']
+    const response = ['<h1>Full Cycle Rocks!!!</h1>']
     for (const person of rows) {
         response.push(`<p>${person.name}</p>`)
     }
